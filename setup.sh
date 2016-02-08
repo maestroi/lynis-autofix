b07f767c75623e0da60dd3597d1b647392aff113
maestroi <chuck@tonec.nl>
2016-02-08 23:10:39 +0100

    test3

--- setup.sh
+++ setup.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
 echo ------------------------
 echo Setup script by meastro
 echo ------------------------
-sudo apt-get update && sudo apt-get upgrade -y
+setup = setup.sh
+if [ ! -f /$setup ]; then
+    echo "File not found!"
+fi
+echo sudo apt-get update && sudo apt-get upgrade -y
 echo ------------------------
 echo update finished
 echo ------------------------
