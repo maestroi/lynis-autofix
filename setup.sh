1ac21a1a9ff27b7e3559bb751db9dd56dd0d9516
maestroi <chuck@tonec.nl>
2016-02-08 23:57:55 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,11 +2,37 @@
 echo ------------------------
 echo Setup script by meastro
 echo ------------------------
-setup=setup.sh
-if [ ! -f $setup ]; then
-    echo "File not found!"
+echo
+echo ------------------------
+echo checking for clean script!
+echo ------------------------
+clean=clean.sh
+if [ -f $clean ]; then
+    echo "$clean found!"
+    rm $clean
+    wget -q https://raw.githubusercontent.com/maestroi/Hardeningdebian/master/clean.sh
+    chmod 755 $clean
+    echo "$clean newest version downloaded!"
+else 
+    echo "$clean does not exist"
+    wget -q https://raw.githubusercontent.com/maestroi/Hardeningdebian/master/clean.sh
+    chmod 755 $clean
+    echo "$clean new version downloaded!"
 fi
-echo sudo apt-get update && sudo apt-get upgrade -y
+echo ------------------------
+echo done checking script!
+echo ------------------------
+
+echo ------------------------
+echo update ubuntu
+echo ------------------------
+
+update=1
+if [ $update -eq 1 ]; then
+    sudo apt-get update && sudo apt-get upgrade -y
+    update=2
+fi
+
 echo ------------------------
 echo update finished
 echo ------------------------
