4324cd6aec3b279d842b5335b7dce63e83864c5a
Maestroi <chuck@tonec.nl>
2016-02-08 23:20:43 +0100

    cleanscript

---
+++ clean.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+echo ------------------------
+echo clean script by meastro
+echo ------------------------
+setup=setup.sh
+if [ -f $setup ]; then
+	echo "$setup found!"
+    rm $setup
+    wget https://raw.githubusercontent.com/maestroi/Hardeningdebian/master/setup.sh
+    chmod 755 setup.sh
+    ./setup.sh
+else 
+    echo "$setup does not exist"
+fi
+echo ------------------------
+echo clean script done
+echo ------------------------
