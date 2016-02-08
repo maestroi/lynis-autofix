1aa091d6f7dcaa53f9da656edad1c66719b9fcd8
Maestroi <chuck@tonec.nl>
2016-02-09 06:02:47 +0800

    test2
    test

--- setup.sh
+++ setup.sh
@@ -2,3 +2,6 @@ echo ------------------------
 echo Setup script by meastro
 echo ------------------------
 sudo apt-get update && sudo apt-get upgrade -y
+echo ------------------------
+echo update finished
+echo ------------------------
