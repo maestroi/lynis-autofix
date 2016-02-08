2047cbbc2d786a259e149a05445c9a22b8f6ae06
maestroi <chuck@tonec.nl>
2016-02-08 23:47:20 +0100

    cleanup final

--- clean.sh
+++ clean.sh
@@ -12,6 +12,9 @@ if [ -f $setup ]; then
     echo "$setup new version downloaded!"
 else 
     echo "$setup does not exist"
+    wget -q https://raw.githubusercontent.com/maestroi/Hardeningdebian/master/setup.sh
+    chmod 755 setup.sh
+    echo "$setup new version downloaded!"
 fi
 echo ------------------------
 echo clean script done
