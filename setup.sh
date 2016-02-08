b1e4298038a42d49ae352748bcfd9f7123f56e0c
maestroi <chuck@tonec.nl>
2016-02-08 23:11:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ echo ------------------------
 echo Setup script by meastro
 echo ------------------------
 setup = setup.sh
-if [ ! -f /$setup ]; then
+if [ ! -f $setup ]; then
     echo "File not found!"
 fi
 echo sudo apt-get update && sudo apt-get upgrade -y
