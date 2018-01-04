507d123089b004659d590ce3106d7a57781cc274
Maestro <chuck@tonec.nl>
2018-01-05 00:23:15 +0100

    add question

--- app.py
+++ app.py
@@ -20,6 +20,38 @@ console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 listtodo = []
 
+def fix_yes_no(question, default="yes"):
+    """Ask a yes/no question via raw_input() and return their answer.
+
+    "question" is a string that is presented to the user.
+    "default" is the presumed answer if the user just hits <Enter>.
+        It must be "yes" (the default), "no" or None (meaning
+        an answer is required of the user).
+
+    The "answer" return value is True for "yes" or False for "no".
+    """
+    valid = {"yes": True, "y": True, "ye": True,
+             "no": False, "n": False}
+    if default is None:
+        prompt = " [y/n] "
+    elif default == "yes":
+        prompt = " [Y/n] "
+    elif default == "no":
+        prompt = " [y/N] "
+    else:
+        raise ValueError("invalid default answer: '%s'" % default)
+
+    while True:
+        sys.stdout.write(question + prompt)
+        choice = raw_input().lower()
+        if default is not None and choice == '':
+            return valid[default]
+        elif choice in valid:
+            return valid[choice]
+        else:
+            sys.stdout.write("Please respond with 'yes' or 'no' "
+                             "(or 'y' or 'n').\n")
+
 def run_shell_command(command_line):
     command_line_args = shlex.split(command_line)
 
@@ -51,7 +83,13 @@ def fixes():
                 data = json.load(data_file)
             for fix in data:
                 for d in fix:
-                    logging.info('%s - %s' % (data[d]['id'], data[d]['command']))
+                     if fix_yes_no('Do you want to install %s - %s?'%(data[d]['id'], data[d]['Description']), default="yes") == True:
+                        logging.info('%s - %s' % (data[d]['id'], data[d]['command']))
+                     else:
+                         logging.warning('%s - %s is not installed' % (data[d]['id'], data[d]['command']))
+
+
+
 
         except:
             logging.critical('%s.json does not excist in the json directory' % todo[0])
@@ -88,6 +126,7 @@ def todolist():
         matches = re.findall(regex, row)
         listtodo.append(matches)
     file.close()
+
 
 def main():
     logging.info("Welcome to Lynis Autofix!")
